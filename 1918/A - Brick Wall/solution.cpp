f6dc731a513e962fd99096cedfee63142c0019e7
Deep_Halder <118346082+DipHldr@users.noreply.github.com>
2026-07-05 10:43:41 +0530

    Add Brick Wall [A] from Codeforces

---
+++ 1918/A - Brick Wall/solution.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int q;
+    cin>>q;
+   
+    while(q--){
+        int n,m;
+        cin>>n>>m;
+        int num=m/2;
+ 
+        cout<<num*n;
+        cout<<endl;
+       
+    }
+        
+}
